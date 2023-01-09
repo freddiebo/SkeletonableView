cd310f86c9cb48daaa4e3a106d19bd2e30506beb
vsbondarev <vsbondarev@fil-it.ru>
2023-01-09 14:48:22 +0500

    Add sample

---
+++ Examples/Sample/Sample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Sample
+//
+//  Created by Vladislav Bondarev on 09.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
